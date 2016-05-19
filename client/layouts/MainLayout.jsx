fd8982784f32990905c89cd96140b513a358e6cd
hasip <hasip.timurtas@gmail.com>
2016-05-20 02:19:59 +0300

    Home page edited for login

--- client/layouts/MainLayout.jsx
+++ client/layouts/MainLayout.jsx
@@ -6,7 +6,14 @@ export const MainLayout = ({content}) => (
         <Nav />
 
         <div className="container">
-            {content}
+            {Meteor.userId() ? content :
+                <div>
+                    <h2>Login with google account, keep your notes and share..</h2>
+                    <h4>For using the application please log in.</h4>
+                    <img src="screen.jpg" width="60%"/>
+                    <img src="phone.jpg" width="25%"/>
+                </div>
+            }
         </div>
     </div>
 )
